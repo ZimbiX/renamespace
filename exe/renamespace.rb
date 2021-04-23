ab5b715a13996d44cc7491c1c895484514d2eed2
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-04-24 01:51:54 +1000

    Add working renamespace script and spec

---
+++ exe/renamespace.rb
@@ -0,0 +1,207 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+# require 'bundler/inline'
+
+# gemfile do
+#   source 'https://rubygems.org'
+#   gem 'clamp'
+#   gem 'facets'
+#   gem 'rainbow'
+# end
+
+require 'fileutils'
+require 'pathname'
+
+require 'clamp'
+require 'facets/string/camelcase'
+require 'rainbow'
+
+class Renamespace
+  def initialize(source_file_path:, destination_file_path:)
+    @source_file_path = source_file_path
+    @destination_file_path = destination_file_path
+  end
+
+  def call
+    renamespace_file
+    move_spec_file
+    expand_relative_requires_within_all_files
+    rename_within_all_files
+    remove_empty_dirs
+  end
+
+  private
+
+  attr_reader :source_file_path, :destination_file_path
+
+  def renamespace_file
+    puts '%s -> %s' % [namespace_for_path(source_file_path), namespace_for_path(destination_file_path)]
+    content_new = renamespace_file_content(File.read(source_file_path))
+    create_directories_to_file(destination_file_path)
+    File.write(destination_file_path, content_new)
+    File.delete(source_file_path)
+  end
+
+  def move_spec_file
+    if !File.exist?(spec_path(source_file_path))
+      puts Rainbow("Warning: spec file missing for #{spec_path(destination_file_path)}").orange
+      return
+    end
+    create_directories_to_file(spec_path(destination_file_path))
+    FileUtils.mv(
+      spec_path(source_file_path),
+      spec_path(destination_file_path),
+    )
+  end
+
+  def expand_relative_requires_within_all_files
+    (all_ruby_file_paths - relative_requires_expansion_exclusions).each do |path|
+      content_orig = File.read(path)
+      content_new =
+        content_orig
+          .gsub(/require_relative '([^']+)'/) do
+            joined_path = File.join(require_for_path(dir_for_file_path(path)), $1)
+            "require '%s'" % Pathname.new(joined_path).cleanpath
+          end
+      File.write(path, content_new) unless content_orig == content_new
+    end
+  end
+
+  def rename_within_all_files
+    all_ruby_file_paths.each do |path|
+      content_orig = File.read(path)
+      content_new =
+        content_orig
+          .gsub(namespace_for_path(source_file_path), namespace_for_path(destination_file_path))
+          .gsub(require_for_path(source_file_path), require_for_path(destination_file_path))
+      File.write(path, content_new) unless content_orig == content_new
+    end
+  end
+
+  def renamespace_file_content(content)
+    content = content.dup
+    source, dest = source_and_dest_namespace_elements_without_common_prefix
+    namespace_element_replacements = dest.reverse.zip(source.reverse)
+    namespace_element_replacements.each_with_index do |(namespace_element_new, namespace_element_old), i|
+      old_parent_namespace = namespace_element_replacements.last(i + 1).map(&:last).reverse.join('::')
+      old_parent_namespace += '::' if old_parent_namespace.length > 0
+      if namespace_element_old
+        # Replace existing namespace
+        content.sub!(/(class|module) #{namespace_element_old}\b( < (\S+))?/) do
+          "#{$1} RENAMESPACED_#{namespace_element_new}" + ($3 ? " < #{old_parent_namespace}#{$3}" : '')
+        end
+      else
+        # Adding new namespace
+        previous_new_namespace_element = namespace_element_replacements[i - 1].first
+        content.sub!(/((class|module) RENAMESPACED_#{previous_new_namespace_element})/, "module RENAMESPACED_#{namespace_element_new}; \\1")
+        content.sub!(/^(end)/, '\1; end')
+      end
+    end
+    content.gsub!('RENAMESPACED_', '')
+    content
+  end
+
+  def relative_requires_expansion_exclusions
+    %w[
+      spec/spec_helper.rb
+      renamespace_spec.rb
+    ]
+  end
+
+  def source_and_dest_namespace_elements_without_common_prefix
+    source = namespace_elements_for_path(source_file_path)
+    dest = namespace_elements_for_path(destination_file_path)
+    source.each do
+      if source.first == dest.first
+        source.shift
+        dest.shift
+      else
+        break
+      end
+    end
+    [source, dest]
+  end
+
+  def namespace_for_path(path)
+    namespace_elements_for_path(path).join('::')
+  end
+
+  def require_for_path(path)
+    path_elements_for_require(path).join('/')
+  end
+
+  def namespace_elements_for_path(path)
+    path_elements_for_require(path)
+      .map(&method(:namespace_element_from_path_element))
+  end
+
+  def path_elements_for_require(path)
+    path.sub(%r{^lib/}, '').chomp('.rb').split('/')
+  end
+
+  def namespace_element_from_path_element(path_element)
+    custom_camelcasings.fetch(path_element) { path_element.upper_camelcase }
+  end
+
+  def custom_camelcasings
+    {
+      'greensync' => 'GreenSync',
+    }
+  end
+
+  def all_ruby_file_paths
+    (Dir.glob('**/*.rb') - %w[invert_namespaces.rb renamespace.rb])
+  end
+
+  def spec_path(path)
+    possible_spec_paths = [
+      path.sub('.rb', '_spec.rb').sub(/^lib/, 'spec'),
+      path.sub('.rb', '_spec.rb').sub(/^lib/, 'spec/lib'),
+    ]
+    if File.exist?(possible_spec_paths[1])
+      possible_spec_paths[1]
+    else
+      possible_spec_paths[0]
+    end
+  end
+
+  def create_directories_to_file(file_path)
+    FileUtils.mkdir_p(dir_for_file_path(file_path))
+  end
+
+  def dir_for_file_path(file_path)
+    file_path.sub(%r{/[^/]+$}, '')
+  end
+
+  def remove_empty_dirs
+    Dir['**/'].reverse_each { |d| Dir.rmdir(d) if Dir.empty?(d) }
+  end
+end
+
+if __FILE__ == $0
+  Clamp do
+    self.description = <<~TEXT
+      Renamespaces a Ruby source file:
+
+      - Moves the file
+      - Updates, to match the new location, the name of the class/module within the file, including its namespacing
+      - Updates usages of the class/module
+      - Updates the path to the file in all requires
+      - Moves the associated spec file
+
+      Class/module namespaces are derived from the paths provided.
+    TEXT
+
+    parameter 'SOURCE_FILE_PATH', 'The current path of the Ruby source code file to renamespace'
+    parameter 'DESTINATION_FILE_PATH', 'The desired desination path of the file'
+
+    def execute
+      Renamespace.new(
+        source_file_path: source_file_path,
+        destination_file_path: destination_file_path,
+      ).call
+    end
+  end
+end
--- spec/renamespace_spec.rb
+++ spec/renamespace_spec.rb
@@ -1,11 +1,158 @@
 # frozen_string_literal: true
 
+require_relative 'renamespace'
+
 RSpec.describe Renamespace do
-  it "has a version number" do
-    expect(Renamespace::VERSION).not_to be nil
+  let(:renamespace) do
+    described_class.new(
+      source_file_path: paths[0],
+      destination_file_path: paths[1],
+    )
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#renamespace_file_content' do
+    context 'when the number of namespaces is the same' do
+      let(:paths) do
+        %w[
+          lib/dex_registration/models/cursor.rb
+          lib/dex_registration/cursors/model.rb
+        ]
+      end
+
+      let(:source_content) do
+        <<~RUBY
+          module DexRegistration
+            module Models
+              class Cursor
+              end
+            end
+          end
+        RUBY
+      end
+
+      let(:expected_result_content) do
+        <<~RUBY
+          module DexRegistration
+            module Cursors
+              class Model
+              end
+            end
+          end
+        RUBY
+      end
+
+      it 'updates the namespacing' do
+        expect(renamespace.send(:renamespace_file_content, source_content)).to eq(expected_result_content)
+      end
+    end
+
+    context 'when the number of namespaces differs - one added' do
+      let(:paths) do
+        %w[
+          lib/dex_registration/tasks/import_registrations_csv.rb
+          lib/dex_registration/registrations/tasks/import_csv.rb
+        ]
+      end
+
+      let(:source_content) do
+        <<~RUBY
+          module DexRegistration
+            module Tasks
+              class ImportRegistrationsCsv
+              end
+            end
+          end
+        RUBY
+      end
+
+      let(:expected_result_content) do
+        <<~RUBY
+          module DexRegistration
+            module Registrations; module Tasks
+              class ImportCsv
+              end
+            end
+          end; end
+        RUBY
+      end
+
+      it 'updates the namespacing, including adding the new namespace' do
+        expect(renamespace.send(:renamespace_file_content, source_content)).to eq(expected_result_content)
+      end
+    end
+
+    context 'when the number of namespaces differs - two added' do
+      let(:paths) do
+        %w[
+          lib/a/b/c/q.rb
+          lib/a/x/y/z/c/q.rb
+        ]
+      end
+
+      let(:source_content) do
+        <<~RUBY
+          module A
+            module B
+              module C
+                module Q
+                end
+              end
+            end
+          end
+        RUBY
+      end
+
+      let(:expected_result_content) do
+        <<~RUBY
+          module A
+            module X; module Y; module Z
+              module C
+                module Q
+                end
+              end
+            end
+          end; end; end
+        RUBY
+      end
+
+      it 'updates the namespacing, including adding the new namespaces' do
+        expect(renamespace.send(:renamespace_file_content, source_content)).to eq(expected_result_content)
+      end
+    end
+
+    context 'with inheritance' do
+      let(:paths) do
+        %w[
+          lib/a/x.rb
+          lib/b/x.rb
+        ]
+      end
+
+      let(:source_content) do
+        <<~RUBY
+          module App
+            module A
+              class X < Super
+              end
+            end
+          end
+        RUBY
+      end
+
+      let(:expected_result_content) do
+        <<~RUBY
+          module App
+            module B
+              class X < A::Super
+              end
+            end
+          end
+        RUBY
+      end
+
+      it 'expands the namespacing of the superclass only as necessary' do
+        expect(renamespace.send(:renamespace_file_content, source_content)).to eq(expected_result_content)
+      end
+    end
   end
 end
