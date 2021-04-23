b58c742088922925047dc9b4991e7af3ade0c7b6
Brendan Weibrecht <brendan@weibrecht.net.au>
2021-04-24 01:48:21 +1000

    Generate new gem
    ```bash
    bundle gem renamespace --exe --no-mit --test=rspec
    ```

---
+++ lib/renamespace.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "renamespace/version"
+
+module Renamespace
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/renamespace_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Renamespace do
+  it "has a version number" do
+    expect(Renamespace::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
